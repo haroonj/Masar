b9ca7f970806afd531088afd207da9614be4f76b
haroonj <jj.haroon99@gmail.com>
2020-09-15 02:21:54 +0300

    initial

---
+++ android/app/src/main/kotlin/com/example/masar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.masar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Masar',
+
+      home: SafeArea(
+        child: Scaffold(
+          body: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/flashwlc.png"),
+                fit: BoxFit.cover,
+              )
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
