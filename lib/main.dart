2688e8273bb8976686c518b9fa07455945266141
haroonj <jj.haroon99@gmail.com>
2020-09-15 08:35:47 +0300

    Merge branch 'master' of https://github.com/haroonj/Masar into master
    # Conflicts:
    #	README.md

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:masar/screens/maptest.dart';
 import 'screens/loading_screen.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
 
       home: SafeArea(
         child: Scaffold(
-          body: LoadingScreen()
+          body: MapSample()
         ),
       ),
     );
---
+++ lib/screens/maptest.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(	32.551445, 	35.851479),
+    zoom: 14.4746,
+  );
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+
+    );
+  }
+
+
+}
