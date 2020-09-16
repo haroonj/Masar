7cd8d5933103e3dd9765a69f454727c734d22e01
haroonj <jj.haroon99@gmail.com>
2020-09-16 14:16:22 +0300

    Merge branch 'master' of https://github.com/haroonj/Masar into master
    # Conflicts:
    #	README.md

--- lib/screens/maptest.dart
+++ lib/screens/maptest.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+import 'package:geolocator/geolocator.dart' as gl;
+import 'package:masar/uti.dart';
+
 
 class MapSample extends StatefulWidget {
   @override
@@ -16,6 +19,12 @@ class MapSampleState extends State<MapSample> {
   Marker marker;
   Circle circle;
   GoogleMapController _controller;
+  String searchAddress;
+
+  List<BussStation> buses = [
+    BussStation(name: 'Amman', lat: 32.530092, lng: 35.870488),
+    BussStation(name: 'Bushra', lat: 32.556068, lng: 35.857133),
+  ];
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(32.551445, 35.851479),
@@ -83,6 +92,46 @@ class MapSampleState extends State<MapSample> {
     }
   }
 
+  void search() {
+
+    if (_locationSubscription != null) {
+      _locationSubscription.cancel();
+    }
+    if(searchAddress == '0'){
+      _controller.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+              target: LatLng(
+                buses[0].lat,
+                buses[0].lng,
+              ),
+              zoom: 18.0
+          ),
+        ),
+      );
+    }
+    else {
+      _controller.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+              target: LatLng(
+                buses[1].lat,
+                buses[1].lng,
+              ),
+              zoom: 18.0
+          ),
+        ),
+      );
+    }
+    matureDistance();
+  }
+
+  void matureDistance ()async{
+    gl.Position position = await gl.getCurrentPosition(desiredAccuracy: gl.LocationAccuracy.high );
+    double distance =  gl.distanceBetween(position.latitude, position.longitude, buses[1].lat, buses[1].lng);
+    print(distance);
+  }
+
   @override
   void dispose() {
     if (_locationSubscription != null) {
@@ -94,19 +143,59 @@ class MapSampleState extends State<MapSample> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        markers: Set.of((marker != null) ? [marker] : []),
-        onMapCreated: (GoogleMapController controller) {
-          _controller = controller;
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.location_searching),
-        onPressed: () {
-          getCurrentLocation();
-        },
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.hybrid,
+            initialCameraPosition: _kGooglePlex,
+            markers: Set.of((marker != null) ? [marker] : []),
+            onMapCreated: (GoogleMapController controller) {
+              setState(() {
+                _controller = controller;
+              });
+            },
+          ),
+          Positioned(
+            left: 40,
+            bottom: 40,
+            child: FloatingActionButton(
+              child: Icon(Icons.location_searching),
+              onPressed: () {
+                getCurrentLocation();
+              },
+            ),
+          ),
+          Positioned(
+            top: 30,
+            right: 15,
+            left: 15,
+            child: Container(
+              height: 50,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Colors.white,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Where to go',
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(left: 15, top: 15),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: search,
+                    iconSize: 30,
+                  ),
+                ),
+                onChanged: (val) {
+                  setState(() {
+                    searchAddress = val;0
+                  });
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/uti.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+
+class BussStation{
+
+  BussStation({@required this.name,@required this.lat,@required this.lng});
+  final String name;
+  final double lat;
+  final double lng;
+
+
+}
